4dae8fb97389a8a9e115bf8e24b58d2699ba306d
jackcase04 <jackcase04@gmail.com>
2026-03-17 10:36:55 -0500

    Fix bug in processor hashmap, add more users, and add haversine function

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,20 @@ fn main() {
     let contents = fs::read_to_string("data/users.json").expect("Should have read file");
     let users: Vec<User> = serde_json::from_str(&contents).unwrap();
 
+    let mut i = 0;
     for user in users {
-        processor.users.insert(1, user);
+        processor.users.insert(i, user);
+        i = i + 1;
     }
 
+    // This will be changed to be read over the network from the load generator side
+    // For now for testing, just read it in from disk
     let contents = fs::read_to_string("data/transactions.json").expect("Should have read file");
     let transactions: Vec<Transaction> = serde_json::from_str(&contents).unwrap();
     
     let approved = processor.process_transaction(&transactions[0]);
 
+    println!("Users: {0}", processor.users.len());
     println!("Approved: {0}", approved);
 
     
--- src/processing.rs
+++ src/processing.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, f32::consts::PI};
 use serde::{Deserialize};
 
 use crate::transaction::Transaction;
@@ -6,7 +6,6 @@ use crate::transaction::Transaction;
 pub struct Processor {
     pub users: HashMap<u32, User>
 }
-
 
 #[derive(Deserialize, Debug)]
 pub struct User {
@@ -25,4 +24,24 @@ impl Processor {
             None => false
         }
     }
+}
+
+fn haversine(lat1: f64, long1: f64, lat2: f64, long2: f64) -> f64{
+    // Using haversine formula:
+    // https://www.geeksforgeeks.org/dsa/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
+
+    let d_lat: f64 = ((lat2 - lat1) * PI as f64 / 180.0);
+    let d_lon: f64 = ((long2 - long1) * PI as f64 / 180.0);
+
+    let temp_lat1 = lat1 * PI as f64 / 180.0;
+    let temp_lat2 = lat2 * PI as f64 / 180.0;
+
+    let a = (d_lat / 2.0).sin().powf(2.0) + 
+                (d_lon / 2.0).sin().powf(2.0) *
+                temp_lat1.cos() * temp_lat2.cos();
+
+    let rad = 6371.0;
+    let c = 2.0 * a.sqrt().asin();
+
+    rad * c
 }
