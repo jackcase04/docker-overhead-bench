9f81142f84b19f695e056a8e42f13c5fc9ec0222
jackcase04 <jackcase04@gmail.com>
2026-03-19 22:04:10 -0500

    Implement basic server functionality

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::fs;
+use std::io::Read;
 use docker_overhead_bench::processing::Processor;
-use docker_overhead_bench::structs::Transaction;
 use docker_overhead_bench::structs::User;
+
+use std::net::TcpListener;
 
 fn main() {
     let mut processor = Processor {
@@ -18,15 +20,29 @@ fn main() {
         i = i + 1;
     }
 
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        println!("Connection established!");
+
+        let mut data = String::new();
+
+        stream.read_to_string(&mut data);
+
+        println!("Data: {0}", data);
+    }
+
     // This will be changed to be read over the network from the load generator side
     // For now for testing, just read it in from disk
-    let contents = fs::read_to_string("data/transactions.json").expect("Should have read file");
-    let transactions: Vec<Transaction> = serde_json::from_str(&contents).unwrap();
+    // let contents = fs::read_to_string("data/transactions.json").expect("Should have read file");
+    // let transactions: Vec<Transaction> = serde_json::from_str(&contents).unwrap();
     
-    let approved = processor.process_transaction(&transactions[0]);
+    // let approved = processor.process_transaction(&transactions[0]);
 
-    println!("Users: {0}", processor.users.len());
-    println!("Approved: {0}", approved);
+    // println!("Users: {0}", processor.users.len());
+    // println!("Approved: {0}", approved);
 
     
 }
