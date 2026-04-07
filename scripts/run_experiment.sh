abf0dc267774d67093c49b30709bdd13df912502
jackcase04 <jackcase04@gmail.com>
2026-04-07 10:53:24 -0500

    Added changes to run experiment for all 3 configurations from bash script

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -9,9 +9,39 @@ for CONCURRENCY in 1 10 50 100 200
 do
   for TRIAL in 1 2 3 4 5
   do
-    echo "Running trial $TRIAL for concurrency $CONCURRENCY:"
+    echo "Running trial $TRIAL for concurrency $CONCURRENCY, config \"bare metal\":"
     ./target/release/loadgen 10000 $CONCURRENCY $TRIAL "bare metal"
   done
 done
 
 kill $SERVER_PID
+
+# CONFIG 2: Docker host mode
+docker run --rm -d --name server1 --network host fraud-server
+sleep 1 
+
+for CONCURRENCY in 1 10 50 100 200
+do
+  for TRIAL in 1 2 3 4 5
+  do
+    echo "Running trial $TRIAL for concurrency $CONCURRENCY, config \"docker host mode\":"
+    ./target/release/loadgen 10000 $CONCURRENCY $TRIAL "docker host"
+  done
+done
+
+docker stop server1
+
+# CONFIG 3: Docker bridge mode
+docker run --rm -d --name server2 -p 7878:7878 fraud-server
+sleep 1
+
+for CONCURRENCY in 1 10 50 100 200
+do
+  for TRIAL in 1 2 3 4 5
+  do
+    echo "Running trial $TRIAL for concurrency $CONCURRENCY, config \"docker bridge mode\":"
+    ./target/release/loadgen 10000 $CONCURRENCY $TRIAL "docker bridge"
+  done
+done
+
+docker stop server2
