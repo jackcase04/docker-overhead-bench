4e48274eeee35d671125b71b7b57d1061aaf38dd
jackcase04 <jackcase04@gmail.com>
2026-04-07 10:36:39 -0500

    Modified script to run server in background then generate load

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # CONFIG 1: No container
+./target/release/server 127.0.0.1 &
+SERVER_PID=$!
+sleep 1
+
 for CONCURRENCY in 1 10 50 100 200
 do
   for TRIAL in 1 2 3 4 5
@@ -9,3 +13,5 @@ do
     ./target/release/loadgen 10000 $CONCURRENCY $TRIAL "bare metal"
   done
 done
+
+kill $SERVER_PID
