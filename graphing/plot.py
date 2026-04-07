1780338e9e2242750b7545b591b91fafac715f44
jackcase04 <jackcase04@gmail.com>
2026-04-07 11:46:10 -0500

    Modified python script to graph all trials and write them to .png

--- graphing/plot.py
+++ graphing/plot.py
@@ -1,24 +1,37 @@
 import matplotlib.pyplot as plt
 import csv
+import os
 
-y = []
+path = "csv"
 
-with open('results.csv', mode='r') as file:
-    csvfile = csv.reader(file)
-    for lines in csvfile:
-        y.append(int(lines[1]))   
+try:
+    os.mkdir("results")
+except:
+    print("don't need to create")
 
-x = []
+for file in os.scandir(path):
+    if file.is_file():
+        y = []
 
-for i in range(0, len(y)):
-    x.append(i)
+        with open(file.path, mode='r') as file:
+            csvfile = csv.reader(file)
+            for lines in csvfile:
+                y.append(int(lines[1]))   
+
+        x = []
+
+        for i in range(0, len(y)):
+            x.append(i)
 
 
-plt.plot(x, y)
+        plt.plot(x, y)
 
-plt.xlabel("Request number")
-plt.ylabel("Latency (us)")
+        plt.xlabel("Request number")
+        plt.ylabel("Latency (us)")
 
-plt.title("Latency of transactions")
+        plt.title("Latency of transactions: " + file.name)
 
-plt.show()
+        files = file.name.split('.')[0].split('/')[1]
+        # plt.show()
+        plt.savefig("results/" + files)
+        plt.clf()
