7be823fda839f56ba46a545ef9168a8e8baa68d7
jackcase04 <jackcase04@gmail.com>
2026-04-09 14:47:52 -0500

    Refactored loadgen main function to avoid repeated logic

--- src/bin/loadgen.rs
+++ src/bin/loadgen.rs
@@ -21,30 +21,12 @@ fn main() {
     let mut threads_sent = 0;
     let mut threads_finished = 0;
 
-    while threads_sent < concurrency {
-        let sender = sender.clone();
-
-        let random = rand::thread_rng().gen_range(0..30) as usize;
-        let trans = transactions[random].clone();
-
-        thread::spawn(move || {
-            let data: Vec<u8> =
-                serde_json::to_vec(&trans).expect("Should have parsed transaction properly");
-
-            let start = Instant::now();
-            send_transaction(data);
-            sender
-                .send((start, start.elapsed()))
-                .expect("Should have sent result");
-        });
-
-        threads_sent += 1;
-    }
-
     while threads_finished < iterations {
-        let result = receiver.recv().expect("Should have receieved result");
-        results.push(result);
-        threads_finished += 1;
+        if threads_sent >= concurrency {
+            let result = receiver.recv().expect("Should have receieved result");
+            results.push(result);
+            threads_finished += 1;
+        }
 
         if threads_sent < iterations {
             let sender = sender.clone();
